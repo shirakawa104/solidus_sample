a3ccd6504ace71dc2704d1ac6a1ab4c1f0b6f249
shirakawa104 <srkwtsak@gmail.com>
2016-06-20 15:42:35 +0900

    add original payment

--- app/models/spree/address_decorator.rb
+++ app/models/spree/address_decorator.rb
@@ -1,6 +1,10 @@
 Spree::Address.class_eval do
   def full_name
     "#{lastname} #{firstname}".strip
-    #"#{firstname} #{lastname}".strip
+    #if config.i18n.default_locale == :ja
+    #  "#{lastname} #{firstname}".strip
+    #else
+    #  "#{firstname} #{lastname}".strip
+    #end
   end
 end
